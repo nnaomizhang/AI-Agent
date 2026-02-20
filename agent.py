cf5edd8a3c44b83670a1925b912b29ba3a15b10f
naomiz11 <naomizhangx@hotmail.com>
2026-02-20 10:59:25 +0000

    Heading

---
+++ agent.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+st.title("Your Market Research Agent")
+st.header("Industry")
+
+
+
